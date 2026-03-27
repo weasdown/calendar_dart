6432d856ee1eee2b7e153ce55c4f85c5bfff5fc2
weasdown <34220924+weasdown@users.noreply.github.com>
2026-03-27 13:15:58 +0000

    docs: add library docs

--- lib/src/calendar.dart
+++ lib/src/calendar.dart
@@ -1,3 +1,11 @@
+/// Calendar printing functions
+///
+/// Note when comparing these calendars to the ones printed by cal(1): By
+/// default, these calendars have Monday as the first day of the week, and
+/// Sunday as the last (the European convention). Use setfirstweekday() to
+/// set the first day of the week (0=Monday, 6=Sunday).
+library;
+
 class IllegalMonthError implements Exception {
   IllegalMonthError(this.month);
 
