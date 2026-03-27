513d7b96b4a82054bb81c2c5bac6cb58fe14f8e6
weasdown <34220924+weasdown@users.noreply.github.com>
2026-03-27 11:21:54 +0000

    feat: add skeleton attribute and methods to Calendar class

--- lib/src/calendar.dart
+++ lib/src/calendar.dart
@@ -1,4 +1,61 @@
 /// Base calendar class. This class doesn't do any formatting. It simply provides data to subclasses.
 class Calendar {
-  Calendar();
+  Calendar({this.firstWeekDay = 0});
+
+  /// 0 = Monday, 6 = Sunday
+  int firstWeekDay;
+
+  int getFirstWeekDay() => firstWeekDay % 7;
+
+  void setfirstweekday(int firstWeekDay) {
+    this.firstWeekDay = firstWeekDay;
+  }
+
+  iterWeekDays() {
+    throw UnimplementedError();
+  }
+
+  iterMonthDates() {
+    throw UnimplementedError();
+  }
+
+  iterMonthDays() {
+    throw UnimplementedError();
+  }
+
+  iterMonthDays2() {
+    throw UnimplementedError();
+  }
+
+  iterMonthDays3() {
+    throw UnimplementedError();
+  }
+
+  iterMonthDays4() {
+    throw UnimplementedError();
+  }
+
+  monthDatesCalendar() {
+    throw UnimplementedError();
+  }
+
+  monthDays2Calendar() {
+    throw UnimplementedError();
+  }
+
+  monthDaysCalendar() {
+    throw UnimplementedError();
+  }
+
+  yearDatesCalendar() {
+    throw UnimplementedError();
+  }
+
+  yearDays2Calendar() {
+    throw UnimplementedError();
+  }
+
+  yearDaysCalendar() {
+    throw UnimplementedError();
+  }
 }
