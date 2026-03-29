66b1c34dcb062bc0c279f6869acc286a4356fe0c
weasdown <34220924+weasdown@users.noreply.github.com>
2026-03-29 11:32:46 +0100

    feat: add extension for converting (nested) lists of DateTimes into (nested) lists of Python-emulating Strings

--- lib/src/date.dart
+++ lib/src/date.dart
@@ -10,3 +10,25 @@ class Date extends DateTime {
   @override
   String toString() => 'Date($year, $month, $day)';
 }
+
+extension WDDateString on List<DateTime> {
+  List<String> toPythonString() => List<String>.from(
+    map((DateTime day) => Date.fromDateTime(day).toPythonString()),
+  );
+}
+
+extension MWDDateString on List<List<DateTime>> {
+  List<List<String>> toPythonString() =>
+      map((List<DateTime> week) => week.toPythonString()).toList();
+}
+
+extension MsWDDateString on List<List<List<DateTime>>> {
+  List<List<List<String>>> toPythonString() =>
+      map((List<List<DateTime>> month) => month.toPythonString()).toList();
+}
+
+extension YMWDDateString on List<List<List<List<DateTime>>>> {
+  List<List<List<List<String>>>> toPythonString() => map(
+    (List<List<List<DateTime>>> months) => months.toPythonString(),
+  ).toList();
+}
