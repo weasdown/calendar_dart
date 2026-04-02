2319366e299fd8b6103f63a641243c44e0cff4cd
weasdown <34220924+weasdown@users.noreply.github.com>
2026-04-02 20:53:51 +0100

    feat: add _LocalizedMonth instances _monthName and _monthAbbr

--- lib/src/calendar.dart
+++ lib/src/calendar.dart
@@ -204,6 +204,10 @@ class _LocalizedMonth {
 final _LocalizedDay _dayName = _LocalizedDay('EEEE');
 final _LocalizedDay _dayAbbr = _LocalizedDay('E');
 
+// Full and abbreviated names of months (1-based arrays!!!)
+final _LocalizedMonth _monthName = _LocalizedMonth('%B');
+final _LocalizedMonth _monthAbbr = _LocalizedMonth('%b');
+
 /// Return [True] for leap years, [False] for non-leap years.
 bool isLeap(int year) => year % 4 == 0 && (year % 100 != 0 || year % 400 == 0);
 
