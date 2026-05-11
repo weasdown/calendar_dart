bc36400f6427506f5d63d4a6b01782e9953df6e4
weasdown <34220924+weasdown@users.noreply.github.com>
2026-05-11 11:21:39 +0100

    fix: fix error message

--- lib/src/errors.dart
+++ lib/src/errors.dart
@@ -1,7 +1,7 @@
 /// Simplifies throwing of [UnimplementedError]s for unimplemented methods.
 class UnimplementedMethodError extends UnimplementedError {
   UnimplementedMethodError(this.className, this.methodName)
-    : super('$className.$methodName() is not yet implemented.');
+    : super('$className.$methodName() method is not yet implemented.');
 
   final String className;
   final String methodName;
