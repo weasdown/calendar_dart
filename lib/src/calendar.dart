8da792ebe3815cbf520a91be732185f519549b20
weasdown <34220924+weasdown@users.noreply.github.com>
2026-05-14 12:23:48 +0100

    docs: remove completed FIXMEs, make comments consistent

--- lib/src/calendar.dart
+++ lib/src/calendar.dart
@@ -555,43 +555,8 @@ final class TextCalendar extends Calendar {
     return s;
   }
 
-  // FIXME fix method
   /// Returns a year's calendar as a multi-line string.
   String formatYear(int theYear, [w = 2, l = 1, c = 6, int m = 3]) {
-    /// w = max(2, w)
-    /// l = max(1, l)
-    /// c = max(2, c)
-    /// colwidth = (w + 1) * 7 - 1
-    /// v = []
-    /// a = v.append
-    /// a(repr(theyear).center(colwidth*m+c*(m-1)).rstrip())
-    /// a('\n'*l)
-    /// header = self.formatweekheader(w)
-    /// for (i, row) in enumerate(self.yeardays2calendar(theyear, m)):
-    ///     # months in this row
-    ///     months = range(m*i+1, min(m*(i+1)+1, 13))
-    ///     a('\n'*l)
-    ///     names = (self.formatmonthname(theyear, k, colwidth, False)
-    ///              for k in months)
-    ///     a(formatstring(names, colwidth, c).rstrip())
-    ///     a('\n'*l)
-    ///     headers = (header for k in months)
-    ///     a(formatstring(headers, colwidth, c).rstrip())
-    ///     a('\n'*l)
-    ///
-    ///     # max number of weeks for this row
-    ///     height = max(len(cal) for cal in row)
-    ///     for j in range(height):
-    ///         weeks = []
-    ///         for cal in row:
-    ///             if j >= len(cal):
-    ///                 weeks.append('')
-    ///             else:
-    ///                 weeks.append(self.formatweek(cal[j], w))
-    ///         a(formatstring(weeks, colwidth, c).rstrip())
-    ///         a('\n' * l)
-    /// return ''.join(v)
-
     w = max(2, w);
     l = max(1, l);
     c = max(2, c);
@@ -604,12 +569,12 @@ final class TextCalendar extends Calendar {
       return v;
     }
 
-    // Equivalent to Python's `a('\n'*l)`.
+    // Python: `a('\n'*l)`.
     void lNewlines() {
       a(List<String>.filled(l, '\n').join(''));
     }
 
-    // `a(repr(theyear).center(colwidth*m+c*(m-1)).rstrip())`
+    // Python: `a(repr(theyear).center(colwidth*m+c*(m-1)).rstrip())`.
     a(
       theYear
           .toString()
@@ -652,10 +617,7 @@ final class TextCalendar extends Calendar {
       }
     }
 
-    return v.join(
-      '',
-    ); // FIXME v not having the correct text added - remains empty
-    // throw UnimplementedMethodError(runtimeType.toString(), 'formatYear');
+    return v.join('');
   }
 
   /// Print a year's calendar.
